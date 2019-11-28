8c0f8643d1d8a6571769e49b0eae9ec2dcc0f160
VielenDanke <vielendanke1991@gmail.com>
2019-11-28 16:49:03 +0600

    commit from work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+import Message from './components/Message'
+import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FunctionClick/>
+      <ClassClick />
+    
+      {/* <Counter /> */}
+
+        {/* <Message/> */}
+        {/* <Greet name="Aliya" heroName="Wonder Woman">
+          <p>This is a children props</p>
+        </Greet>
+        <Greet name="Vlad" heroName="Batman">
+          <button>Action</button>
+        </Greet> */}
+        {/* <Greet name="Almat" heroName="Superman" />
+        <Welcome name="Bruce" heroName="Batman"/> */}
+        {/* <Welcome name="Clarc" heroName="Superman"/>
+        <Welcome name="Diana" heroName="Wonder Woman"/> */}
+        {/* <Hello/> */}
     </div>
   );
 }
---
+++ src/components/Greet.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// function Greet () {
+//     return (
+//         <h1>Hi</h1>
+//     )
+// }
+
+const Greet = (props) => {
+    const {name, heroName} = props 
+    return (
+        <div>
+            <h1> 
+                Hello {name} a.k.a {heroName}
+            </h1>
+            {/* {props.children} */}
+        </div>
+    )
+}
+
+export default Greet
